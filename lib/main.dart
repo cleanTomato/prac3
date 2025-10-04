6b1a7e1c2205937a3618a88b56488e2ef5a2c5a3
Anzdock <anzdock-azndock200@yandex.ru>
2025-10-04 09:21:33 +0300

    Revert "вариативная часть практической работы"
    This reverts commit eaf775cf5ce4a372b912bf29e2a28795074e9ca8.

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,47 @@
 import 'package:flutter/material.dart';
-import 'Screens/home_screen.dart';
-
-
+//Мыслительные процессы
 void main() {
   runApp(const MyApp());
 }
-
+//Мыслительные процессы
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Task Manager',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.dark,
-        ),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+//Мыслительные процессы
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+//Мыслительные процессы
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+//Мыслительные процессы
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
+//Мыслительные процессы
